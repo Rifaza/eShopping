6db7ebfae632d6b5fdb62d3d82e44a436c9ee031
Anas Fathima Rifaza <afrifaza17@gmail.com>
2024-12-25 17:09:41 +0530

    added notification

--- src/app/Container/container.component.ts
+++ src/app/Container/container.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "../nav/nav.component";
 import { HeaderComponent } from "../header/header.component";
+import { NotificationComponent } from "../notification/notification.component";
 
 @Component({
   selector: 'app-container',
-  imports: [RouterOutlet, NavComponent, HeaderComponent],
+  imports: [RouterOutlet, NavComponent, HeaderComponent, NotificationComponent],
   templateUrl:'./container.component.html',
   styleUrls : ['./container.component.css']
 })
